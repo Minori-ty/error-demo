e5ec78e8db9fe188a52682b27bc39ee793bc657d
守夜人 <1229248506@qq.com>
2021-12-24 10:58:31 +0800

    remove cdn

--- comic-ts/vite.config.ts
+++ comic-ts/vite.config.ts
@@ -7,12 +7,27 @@ import externalGlobals from 'rollup-plugin-external-globals'
 import html from 'vite-plugin-html'
 import viteCompression from 'vite-plugin-compression'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import importToCDN, { autoComplete } from 'vite-plugin-cdn-import'
+import ElementPlus from 'element-plus'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [
         vue(),
         vueJsx(),
+        importToCDN({
+            modules: [
+                autoComplete('vue'), // vue2 使用 autoComplete('vue2')
+                // autoComplete('@vueuse/shared'),
+                // autoComplete('@vueuse/core'),
+                // {
+                //     name: 'element-plus',
+                //     var: 'ElementPlus',
+                //     path: `/lib/index.full.js`,
+                // },
+            ],
+        }),
         // AutoImport({
         //     resolvers: [ElementPlusResolver()],
         // }),
